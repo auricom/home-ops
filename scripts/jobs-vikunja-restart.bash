6032bd04f90a043487aabc8dc287d9333d910bef
auricom <github@xpander.eml.cc>
2021-04-15 13:26:12 +0200

    move script to cronjob yaml definition

--- scripts/jobs-vikunja-restart.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -x
-
-result=$(kubectl get pod --selector app.kubernetes.io/name=vikunja --output custom-columns=:metadata.name --namespace data)
-VIKUNJA_POD=$(echo $result | awk '{ print $NF }')
-echo $VIKUNJA_POD | grep vikunja
-test $? -eq 0 && kubectl delete pod $VIKUNJA_POD --namespace data
