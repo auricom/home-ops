23aa291db9c30bba47d882ec884c7f21cb909cb0
auricom <27022259+auricom@users.noreply.github.com>
2022-12-17 23:35:52 +0100

    ⚡️ improve truenas cert deploy script

--- infrastructure/ansible/roles/truenas/templates/scripts/certificates_deploy.bash
+++ infrastructure/ansible/roles/truenas/templates/scripts/certificates_deploy.bash
@@ -3,31 +3,35 @@
 # DEBUG
 # set -x
 
-# Get certificates from remote server
-
 # Variables
+TARGET=$(hostname)
+DAYS=21
 SCRIPT_PATH="{{ scripts_dir }}"
 CERTIFICATE_PATH="{{ certificates_dir }}"
 CONFIG_FILE="${SCRIPT_PATH}/certificates_deploy.conf"
-{% if main_nas == true %}POSTGRES_DIR="/mnt/storage/jail-mounts/postgres/data{{ postgres_version }}/"{% endif %}
+UPTIME_KUMA_ID="{{uptime_kuma_id_truenas_cert}}"
 
-# Check if cert has been uploaded last week
-result=$(find ${CERTIFICATE_PATH}/cert.pem -mtime -7)
+# Check if cert is older than 69 days
+result=$(find ${CERTIFICATE_PATH}/cert.pem -mtime +69)
 
 if [[ "$result" == "${CERTIFICATE_PATH}/cert.pem" ]]; then
-
-    # Deploy certificate (truenas UI & minio)
-    python ${SCRIPT_PATH}/certificates_deploy.py -c ${CONFIG_FILE}
-    test $? -ne 0 && FLAG_NOTIF=true
-    {% if main_nas == true %}
-
-    # Deploy certificate (postgresql jail)
-    umask 0177
-    cp ${CERTIFICATE_PATH}/fullchain.pem ${POSTGRES_DIR}/server.crt
-    cp ${CERTIFICATE_PATH}/key.pem ${POSTGRES_DIR}/server.key
-    chown 770:770 ${POSTGRES_DIR}/server.crt ${POSTGRES_DIR}/server.key
-    chmod 600 ${POSTGRES_DIR}/server.crt ${POSTGRES_DIR}/server.key
-    # restart postgresql
-    iocage postgres service postgresql restart
-    {% endif %}
+    echo "ERROR - Certificate is older than 69 days"
+    echo "ERROR - Verify than it has been renewed by ACME client on opnsense and that the upload automation has been executed"
+    curl -s \
+        --form-string "token={{secret_pushover_api_key}}" \
+        --form-string "user={{secret_pushover_user_key}}" \
+        --form-string "message=Certificate on $TARGET is older than 69 days. Verify than it has been renewed by ACME client on opnsense and that the upload automation has been executed" \
+        https://api.pushover.net/1/messages.json
+else
+    echo "checking if $TARGET expires in less than $DAYS days"
+    openssl x509 -checkend $(( 24*3600*$DAYS )) -noout -in <(openssl s_client -showcerts -connect $TARGET:443 </dev/null 2>/dev/null | openssl x509 -outform PEM)
+    if [ $? -ne 0 ]; then
+        echo "INFO - Certificate expires in less than $DAYS days"
+        echo "INFO - Deploying new certificate"
+        # Deploy certificate (truenas UI & minio)
+        python ${SCRIPT_PATH}/certificates_deploy.py -c ${CONFIG_FILE}
+        test $? -eq 0 && curl https://uptime-kuma.{{secret_cluster_domain}}/api/push/${UPTIME_KUMA_ID}?status=up&msg=OK&ping=
+    else
+        echo "INFO - Certificate expires in more than $DAYS"
+    fi
 fi
