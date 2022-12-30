e4a4323b499f150811580af1d20192de708041d8
auricom <27022259+auricom@users.noreply.github.com>
2022-12-30 18:38:54 +0100

    🔧 truenas scripts

--- ansible/roles/truenas/templates/scripts/snapshots_prune.sh
+++ ansible/roles/truenas/templates/scripts/snapshots_prune.sh
@@ -11,7 +11,8 @@ POOL_NAME="{{ pool_name }}"
 # Prune
 
 ${SCRIPT_PATH}/snapshots_prune.py --recursive --intervals ${INTERVAL} ${POOL_NAME}
-${SCRIPT_PATH}/snapshots_clearempty.py --recursive ${POOL_NAME}
+${SCRIPT_PATH}/snapshots_prune.py --recursive --intervals daily:14 {% if not main_nas %}replication/{% endif %}${POOL_NAME}/minio
 {% if ansible_facts['nodename'] == "truenas.{{ secret_domain }}" %}
-${SCRIPT_PATH}/snapshots_prune.py --recursive --intervals daily:14 storage/video
+${SCRIPT_PATH}/snapshots_prune.py --recursive --intervals daily:7 ${POOL_NAME}/video
 {% endif %}
+${SCRIPT_PATH}/snapshots_clearempty.py --recursive ${POOL_NAME}
