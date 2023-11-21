8bbb6c6c683224d0f215acf492ca105ff7288e4c
auricom <27022259+auricom@users.noreply.github.com>
2023-11-21 23:04:39 +0100

    🔥 remove terraform

--- terraform/storage/minio/providers.tf
+++
@@ -1,7 +0,0 @@
-provider "minio" {
-  minio_server   = data.sops_file.secrets.data["minio_server"]
-  minio_user     = data.sops_file.secrets.data["minio_root_user"]
-  minio_password = data.sops_file.secrets.data["minio_root_password"]
-  minio_region   = "us-east-1"
-  minio_ssl      = true
-}
